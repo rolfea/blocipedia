fdd9c2503340a310d11110e718cb508b5c93dcd9
Alex Rolfe <rolfepercussion@gmail.com>
2016-02-12 12:44:25 -0600

    Installs pundit and creates user_policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  private
+
+    def user_not_authorized
+      flash[:alert] = "Access denied."
+      redirect_to (request.refer) || root_path)
+    end
 end
