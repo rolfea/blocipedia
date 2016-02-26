4bc034b6d977455144311374485b48fb45e281ab
Alex Rolfe <rolfepercussion@gmail.com>
2016-02-25 20:13:31 -0600

    Adds to Collaborators Controller

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,7 +1,16 @@
 class CollaboratorsController < ApplicationController
   def create
+    @wiki = Wiki.find(params[:wiki_id])
+    collaborator = Collaborator.new(user_id: params[:collaborator], wiki_id: params[:wiki_id])
     puts params[:collaborator]
-    raise
+
+    if collaborator.save
+      flash[:notice] = "Collaborator was saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error adding the collaborator. Please try again."
+      render :edit
+    end
   end
 
   def destroy
