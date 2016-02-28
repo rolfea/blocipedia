9c4694f15bba85c37a726a0e3b8791908790be26
Alex Rolfe <rolfepercussion@gmail.com>
2016-02-27 21:19:35 -0600

    Fixed collaborator controller

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,16 +1,21 @@
 class CollaboratorsController < ApplicationController
   def create
     @wiki = Wiki.find(params[:wiki_id])
-    collaborator = Collaborator.new(user_id: params[:collaborator], wiki_id: params[:wiki_id])
-    puts params[:collaborator]
-
-    if collaborator.save
-      flash[:notice] = "Collaborator was saved."
-      redirect_to @wiki
+    user = User.where(email: params[:collaborator]).first
+    # if user and no collaborator for that user and wiki, create a collaborator
+    if user && !@wiki.users.include?(user)
+      Collaborator.create(user: user, wiki: @wiki)
+      flash[:success] = "#{user.email} was added as a collaborator."
+    # if user and already collaborator, flash error and redirect_to wiki
+    elsif @wiki.users.include?(user)
+      flash[:alert] = "User is already a collaborator"
+    elsif !user
+      flash[:error] = "#{params[:collaborator]} must be a user to add as collaborator"
+    # if !user, then flash error and redirect_to edit for wiki
     else
-      flash.now[:alert] = "There was an error adding the collaborator. Please try again."
-      render :edit
+      flash[:error] = "Something went wrong. Try again, please."
     end
+    redirect_to edit_wiki_path(@wiki)
   end
 
   def destroy
--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,4 +1,6 @@
 class Collaborator < ActiveRecord::Base
   belongs_to :user
   belongs_to :wiki
+
+  validates :user, presence: true
 end
